b70555c51a005d9d3033b86bdb5b57a35e312b4c
Danny Titkov <groverdk@gmail.com>
2026-05-31 16:35:31 +1000

    add requirements.txt, complete no_airflow_app.py

--- no-airflow/no_airflow_app.py
+++ no-airflow/no_airflow_app.py
@@ -1,15 +1,23 @@
+from datetime import date
+
 import requests
 import pandas as pd
 import psycopg2
+from clickhouse_driver import Client
 
 
 def fetch_data_from_github():
+    '''Возвращает список с топ-100 репозиториев на GitHub.'''
+
     GH_REPO_SEARCH_URL = 'https://api.github.com/search/repositories'
     TOP_100_PARAMS = {
-        'q': 'a',
+        # В параметре q делаем фильтр по минимальному количеству звезд,
+        # чтобы в ответе вернулся полный результат (incomplete_results: false).
+        'q': 'stars:>20000',
         'sort': 'stars',
         'order': 'desc',
-        'per_page': '100'
+        'per_page': 100,
+        'page': 1
     }
 
     repos = []
@@ -46,6 +54,12 @@ def fetch_data_from_github():
 
 
 def transform_and_load_to_postgres(repos):
+    '''Функция трансформации и загрузки данных в Postgres.
+
+    Берет список с топ-100 репозиториев, переносит его в DataFrame
+    и загружает DataFrame в таблицу raw_repositories в БД PostgreSQL.
+    '''
+
     extracted_data = []
 
     try:
@@ -105,10 +119,11 @@ def transform_and_load_to_postgres(repos):
         cursor = connection.cursor()
 
         insert_query = '''
-            INSERT INTO raw_repositories
-            (id, name, full_name, owner_login, stargazers_count, created_at)
-            VALUES (%s, %s, %s, %s, %s, %s)
-            ON CONFLICT (id) DO NOTHING
+        INSERT INTO raw_repositories
+        (id, name, full_name, owner_login, stargazers_count, created_at)
+        VALUES (%s, %s, %s, %s, %s, %s)
+        ON CONFLICT (id) DO UPDATE SET
+            stargazers_count = EXCLUDED.stargazers_count
         '''
 
         data_to_insert = [
@@ -140,8 +155,118 @@ def transform_and_load_to_postgres(repos):
     return
 
 
+def aggregate_and_push_to_clickhouse():
+    '''Функция агрегации данных из Postgres и их загрузки в CH.
+
+    Берет агрегированные данные из таблицы raw_repositories в БД PostgreSQL
+    и загружает их в таблицу repo_analytics в БД ClickHouse.
+    '''
+
+    try:
+        postgres_connection = psycopg2.connect(
+            host='localhost',
+            port=5434,
+            user='postgres',
+            password='postgres',
+            database='stats_db'
+        )
+        postgres_cursor = postgres_connection.cursor()
+
+        select_query = '''
+        SELECT
+            owner_login,
+            AVG(stargazers_count) as avg_stars_per_repo,
+            SUM(stargazers_count) as total_stars,
+            COUNT(*) as repo_count
+        FROM raw_repositories
+        GROUP BY owner_login
+        '''
+
+        postgres_cursor.execute(select_query)
+        aggregated_data = postgres_cursor.fetchall()
+
+        if not aggregated_data:
+            raise ValueError(
+                'Запрос в БД PostgreSQL вернул пустой результат.'
+            )
+
+        clickhouse_client = Client(
+            host='localhost',
+            port=9000,
+            user='default',
+            password='default',
+            database='default'
+        )
+
+        current_date = date.today()
+
+        existing_logins_result = clickhouse_client.execute(
+            'SELECT owner_login FROM repo_analytics'
+        )
+        existing_logins = {row[0] for row in existing_logins_result}
+
+        to_update = []
+        to_insert = []
+
+        for record in aggregated_data:
+            owner_login, avg_stars_per_repo, total_stars, repo_count = record
+            if owner_login in existing_logins:
+                to_update.append(record)
+            else:
+                to_insert.append(record)
+
+        if to_update:
+            owner_logins_to_update = [row[0] for row in to_update]
+            login_list = ', '.join([f"'{login}'" for login in
+                                    owner_logins_to_update])
+            delete_query = f'''
+            ALTER TABLE repo_analytics DELETE
+            WHERE owner_login IN ({login_list})
+            '''
+            clickhouse_client.execute(delete_query)
+
+            to_insert.extend(to_update)
+
+        if to_insert:
+            insert_query = '''
+            INSERT INTO repo_analytics (owner_login, avg_stars_per_repo,
+            total_stars, repo_count, updated_at)
+            VALUES
+            '''
+
+            data_to_insert = [
+                (
+                    owner_login,
+                    float(avg_stars_per_repo),
+                    total_stars,
+                    repo_count,
+                    current_date
+                )
+                for owner_login, avg_stars_per_repo, total_stars,
+                repo_count in to_insert
+            ]
+
+            clickhouse_client.execute(insert_query, data_to_insert)
+
+    except Exception as e:
+        raise Exception(
+            'Ошибка в ходе агрегации данных и их загрузки в БД ClickHouse: '
+            f'{e}'
+        )
+
+    finally:
+        if postgres_cursor:
+            postgres_cursor.close()
+        if postgres_connection:
+            postgres_connection.close()
+        if clickhouse_client:
+            clickhouse_client.disconnect()
+
+
 if __name__ == '__main__':
     data = fetch_data_from_github()
     print('fetch_data_from_github — OK')
     transform_and_load_to_postgres(data)
     print('transform_and_load_to_postgres — OK')
+    aggregate_and_push_to_clickhouse()
+    print('aggregate_and_push_to_clickhouse — OK')
